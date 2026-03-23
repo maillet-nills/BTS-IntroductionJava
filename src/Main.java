8fe169414611b731a91ba26e451f4a577003a2e0
Nills Maillet <mailletnills@gmail.com>
2026-03-23 15:07:55 +0100

    done: Exercice 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    // Exercice 1
+    char letter = 'a';
+    int number = 0;
+    long long_number = 1111111111;
+    float float_number = 0.5F;
+    double double_number = 1111111111.1111111111;
+    boolean bool_variable = true;
+    String str = "hello, world";
 
+    System.out.println(letter);
+    System.out.println(number);
+    System.out.println(long_number);
+    System.out.println(float_number);
+    System.out.println(double_number);
+    System.out.println(bool_variable);
+    System.out.println(str);
 }
