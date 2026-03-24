99098f9cf289a46c196c3ab679dc031cbffb3800
Nills Maillet <mailletnills@gmail.com>
2026-03-24 13:47:26 +0100

    done: Exercice 7

--- src/Main.java
+++ src/Main.java
@@ -58,4 +58,11 @@ void main() {
     for (int i = 0; i < 11; i++){
         System.out.println(i);
     }
+
+    // Exercice 7
+    int i = 0;
+    while (i < 11){
+        System.out.println(i);
+        i++;
+    }
 }
